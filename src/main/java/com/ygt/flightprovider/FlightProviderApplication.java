9388c5373bfe0489dcf81a008a3f1ebdf5146ac6
anil <yigit.anil@adesso.com.tr>
2019-03-02 14:31:26 +0300

    Initial commit

---
+++ src/main/java/com/ygt/flightprovider/FlightProviderApplication.java
@@ -0,0 +1,13 @@
+package com.ygt.flightprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightProviderApplication.class, args);
+	}
+
+}
